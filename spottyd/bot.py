c0532d203e05d296a87242a99d2f2d8d9e4bc054
Chris Evans <cwevans612@gmail.com>
2021-08-05 14:46:44 -0700

    Added boilerplate spotify middleware

--- spottyd/bot.py
+++ spottyd/bot.py
@@ -1,79 +1,17 @@
 #!/usr/bin/env python
 import os
-import random
-import re
-
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import discord
 
-
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents = intents)
-
-def getSession(ctx):
-    return bot.SESSIONS[ctx.guild.name]
-
-# FIXME : not threadsafe
-def setSession(ctx, session):
-    bot.SESSIONS[ctx.guild.name] = session
-
-async def tard_bin(user):
-    # Kicks for now
-    session = getSession(user)
-    session["TARDS"][user.discriminator]["count"] = 0
-    await user.kick(reason="TARD BINNN")
-
-
-@bot.event
-async def on_ready():
-    # TODO add tard
-    print(f'{bot.user.name} has connected to Discord!')
-
-    # TODO classify this
-    bot.SESSIONS = {
-        "FMTG" : {
-            "TARDS": {
-                "8545":{
-                    'max_warn': 3,
-                    'count_warn':0,
-                }
-            },
-            "PHOTO_CHANNEL_ID": 785322697633693696
-        },
-        "Cumdog Millionaires":{
-            "TARDS": {
-                "5037":{
-                    'max_warn': 3,
-                    'count_warn':0,
-                }
-            },
-            "PHOTO_CHANNEL_ID": 605860350629380190
-        }
-    }
-
-    for guild in bot.guilds:
-        print("Guild: "+ guild.name)
-        for member in guild.members:
-            print("\t - " +member.name);
-
-@bot.event
-async def on_message(msg):
-    session = getSession(msg)
-    print(msg)
-
-
-@bot.command(name='command', help="Use this command to set the photo")
-async def setPhotoChannel(ctx):
-    session = getSession(ctx)
-    session["PHOTO_CHANNEL_ID"] = ctx.channel.id
-    setSession(ctx, session)
-    await ctx.channel.send("*Photo Channel Set*")
-
+from src.spotify import SpotifyMiddleware
 
 if __name__=="__main__":
-    print("Bot Started")
-    
+    print("\n=========== RESTART ============\n")
+    load_dotenv()
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    intents = discord.Intents.all()
+    bot = commands.Bot(command_prefix='!', intents = intents)
+    bot.add_cog(SpotifyMiddleware(bot))
     bot.run(TOKEN)
+    print("Bot Started")
---
+++ spottyd/src/spotify.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+class SpotifyMiddleware(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener('on_ready')
+    async def on_ready(self):
+        """ Initializes the Database and the Spotify API"""
+
+        print(f'{self.bot.user.name} has connected to Discord with SpotifyMiddleware!')
+        for guild in self.bot.guilds:
+            print("Guild: "+ guild.name)
+            for member in guild.members:
+                print("\t - " +member.name);
+        #TODO
+
+    @commands.Cog.listener('on_message')
+    async def on_message(self, message):
+        """ This is an on_message listener which listens for music recommendations"""
+        # TODO
+        # if(is_spotify_rec(message)):
+        #    add_to_database_and_playlist(message)
+
+    @commands.command()
+    async def set_channel(self, ctx, *, member: discord.Member = None):
+        """Sets as the music recommendation channel"""
+        # TODO
+        await ctx.send(f"{ctx.channel} set as music recs channel!")
